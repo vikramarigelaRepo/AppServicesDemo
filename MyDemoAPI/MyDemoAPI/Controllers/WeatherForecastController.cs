8e41f428fc6885f6ec379d6a4bcc2c591e3eb4d2
vikram arigela <nagavenkatavikram.arigela@cognizant.com>
2020-06-08 13:28:55 -0500

    first change

--- MyDemoAPI/MyDemoAPI/Controllers/WeatherForecastController.cs
+++ MyDemoAPI/MyDemoAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace MyDemoAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Humid","Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
